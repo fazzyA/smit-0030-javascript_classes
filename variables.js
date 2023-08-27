c76024f5709fcdda152c40a368553847cb89017a
da <fa@gmail.com>
2023-08-27 11:06:17 +0500

    first commit

---
+++ variables.js
@@ -0,0 +1,33 @@
+var _name = "";
+var myClassNum = "";
+var mk = 10;
+var $abbb = '';
+
+console.log('my js class');
+var name ="my name is muneeba";
+var myclass = "batch 10";
+// alert(name + " " + myclass);
+
+var num = 100;
+console.log("num * 10" + " " + num * 10);// 1000
+// //console.log("num + 10" + " ", num + 10); //110
+// console.log("num / 10" + " " + num / 10);  //10 
+// console.log("num % 10" + " " + num % 10); // 0
+// console.log("num + '10'" + " " + num + '10'); // 10010
+// console.log("num + '10' - 2" + " " , num + '10' - 2); // 10008 
+// console.log("num + 'ten'" + " " , num + 'ten'); // 100ten 
+// console.log("num - 'ten'" + " ", num - 'ten'); //  NAN
+// console.log(5-(2+4));
+// console.log('10' * 2); // 8
+// console.log('ten' - 2); // NAN
+// + - * % /
+var percentage = prompt("enter your %", 90);
+var total = prompt("enter your total # of question", 40);
+var answer = percentage /100 * total;
+console.log(answer);
+console.log("i got", percentage,"%","and have done",answer, "answers corectly")
+document.write("i got", percentage,"%","and have done",answer, "answers corectly")
+
+
+
+
