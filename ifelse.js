2e30c60a29aaef500e0de3a79610ac623e5ef0b9
faizaGS <faiza@groupshop.com>
2023-08-31 11:03:54 +0500

    prompt n ifelse n operators

---
+++ ifelse.js
@@ -0,0 +1,43 @@
+//ifelse statement
+// var num = +prompt("enter any number");
+
+// if (condition => yes/no === true/false) {
+
+// }
+// if(num == 10) {
+// // do work here
+// console.log("I am in if block");
+// } else {
+// console.log("I am in else block");
+// }
+
+// even odd
+    // var evenOdd = +prompt(); //'3' => 3
+    // // 2 4
+    // if(evenOdd % 2 == 0){
+    //     console.log("you entered even numb")
+    // } else {
+    //     console.log("you entered odd num")
+    // }
+
+
+// write a program to check age > or eq to 18
+    // var age = prompt();
+    // if(age >= 18){
+    // console.log("u r allowed")
+    // } else {
+    //     console.log("not allowed")
+    // }
+    // console.log("after if else block")
+var city = 'Karachi';
+if (city === 'Karachi') {
+   console.log("beautiful city")
+} else{
+    console.log('no karachi')
+}
+var flower = prompt('enter ur flower', 'rose');
+if(flower.toLowerCase() !== 'rose') {
+    console.log('I will accept', flower)
+} else {
+    console.log('will not accept', flower)
+}
---
+++ prompt.js
@@ -0,0 +1,14 @@
+// user input
+// input
+// var a; //declare
+// a = 5; //assign
+// var num = +prompt("Enter your number", 10); // assigining the variable
+// console.log("my num =", num + 2);
+// write a program which takes user age and
+// tell when they will be 50 years old. answer should be in form of
+// year 2040
+var age = +prompt("enter your age"); //30
+var year = 2023;
+var diff = 50 - age; //20 saal bad
+var _year = 2023 + diff;
+console.log("you will be 50 years old in year", _year);
