1a7c244d5b06e0a5e78cb7a081fba2b107459b72
fazzyA <d>
2023-11-16 10:53:21 +0500

    dynamic button

--- todoApp.js
+++ todoApp.js
@@ -1,10 +1,35 @@
 
+console.log(document.querySelector("h1"));
+const eles = document.querySelectorAll(".output");
+const elesClass = document.getElementsByClassName("output");
+console.log(eles);
+console.log(elesClass);
+
+////////////
+const ul = document.getElementById("myul");
+ul.setAttribute("class", "list")
+ul.style.color = "brown"
+ul.style.border = "1px solid pink"
+ul.style.backgroundColor = "pink"
+
+/// create a button thru JS and add onclick method on it
+const newBtn = document.createElement("button")
+newBtn.setAttribute("id", "btn3")
+// newBtn.setAttribute("onClick", "showName()")
+newBtn.addEventListener("click", showName)
+newBtn.textContent = "show my name"
+const parent =  document.querySelector("div")
+parent.appendChild(newBtn)
+
+
+
 function showName() {
     console.log("i am clicked");
     const h1 = document.createElement("h1");
     h1.textContent = "Faiza Aziz Khan"
     const paretnDiv = document.getElementById("container")
-    paretnDiv.appendChild(h1)
+    const myul = document.getElementById("myul")
+    paretnDiv.insertBefore(h1, myul)
 }
 function addList() {
     console.log("iam in addlist");
