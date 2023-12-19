98126c0137da6f8f65d70cbd4f9b02bc0bbf00be
fazzyA <faz.pak@gmail.com>
2023-12-19 09:14:55 +0500

    spread n rest

---
+++ advjs/spread-rest.js
@@ -0,0 +1,31 @@
+// page 151 js to professional book
+var one = [1, 2, 3]
+var two = [4, 5, 6]
+// var three = one + two
+// console.log("🚀 ~ file: spread.js:4 ~ three:", typeof(three), three)
+// question whats wrong in above array declaration one and two
+// var combineArray = 
+// console.log([...one, ...two])
+//web mdn excersize
+
+
+function display(param) {
+    document.write(param)
+}
+
+display('Hi i need you to print me')
+
+
+
+
+// solution
+// function display(param, ...param2) {
+//     document.write(param, ...param2)
+// }
+
+// display('Hi i need you to print me', ' also me', ' and me too')
+
+// function displayArr(arr, ...b){
+//     console.log(arr, b)
+// }
+// displayArr(1, 2, 3)
